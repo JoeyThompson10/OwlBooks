cdc2d46d0aa726056365fc65db534645770345c0
JoeyThompson10 <108598229+JoeyThompson10@users.noreply.github.com>
2023-09-16 17:49:01 -0400

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -6,6 +6,8 @@ const Login = () => {
     const navigate = useNavigate();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [newUsername, setNewUsername] = useState('');
+    const [newPassword, setNewPassword] = useState('');
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [address, setAddress] = useState('');
@@ -19,22 +21,22 @@ const Login = () => {
     async function loginButton(e) {
         e.preventDefault();
         
-        var request = await LoginFunction(username, password);
+        var response = await LoginFunction(username, password);
 
-        if (request.message === "Login successful.") {
+        if (response.message === "Login successful.") {
             navigate("/Dashboard");
         }
         
-        window.alert(request.message);
+        window.alert(response.message);
     }
 
     async function createNewUser(e) {
         e.preventDefault();
 
-        const response = await CreateUser(username, password);
+        const response = await CreateUser(newUsername, newPassword);
         
-        if (response && response.message === "Success") {
-            window.alert("User created!");
+        if (response && response.message.includes("Success")) {
+            window.alert("User created. Please login.");
             window.location.reload();
         } else {
             window.alert("Failed to create user!");
@@ -71,21 +73,29 @@ const Login = () => {
 
             <button onClick={()=>toggleNewUserForm()}>Create New User</button>
             <form id="newUserForm" onSubmit={createNewUser} style={{display: "none"}}>
+            <div className="form-group">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="newUsername" value={newUsername} onChange={e => setNewUsername(e.target.value)} required/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="newPassword" value={newPassword} onChange={e => setNewPassword(e.target.value)} required/>
+                </div>
                 <div className="form-group">
                     <label htmlFor="firstName">First Name</label>
-                    <input type="text" id="firstName" value={firstName} onChange={e => setFirstName(e.target.value)} required/>
+                    <input type="text" id="firstName" value={firstName} onChange={e => setFirstName(e.target.value)}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="lastName">Last Name</label>
-                    <input type="text" id="lastName" value={lastName} onChange={e => setLastName(e.target.value)} required/>
+                    <input type="text" id="lastName" value={lastName} onChange={e => setLastName(e.target.value)}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="address">Address</label>
-                    <input type="text" id="address" value={address} onChange={e => setAddress(e.target.value)} required/>
+                    <input type="text" id="address" value={address} onChange={e => setAddress(e.target.value)}/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="dob">Date of Birth</label>
-                    <input type="date" id="dob" value={dob} onChange={e => setDob(e.target.value)} required/>
+                    <input type="date" id="dob" value={dob} onChange={e => setDob(e.target.value)}/>
                 </div>
                 <button type="submit">Create User</button>
             </form>

6eb0a53364a99d43b2b4bce733950c7e211b80e3
Owen Murphree <90977322+shhhmatic@users.noreply.github.com>
2023-09-21 21:06:54 -0400

    Update AdminDashboard.jsx

--- src/AdminDashboard.jsx
+++ src/AdminDashboard.jsx
@@ -14,6 +14,22 @@ const AdminDashboard = () => {
             <p>
                 This is the admin dashboard page. System admins are able to complete admin specific tasks and abilities from here.
             </p>
+
+            <p>
+                Insert username below to make changes.
+            </p>
+
+            <form>
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    {/*<input type="text" id="username"></input>*/}
+                    <input type="text" class="form-control mb-4" error="wrong" success="right" value=""></input>
+                    <button class="ripple ripple-surface ripple-surface-light btn btn-primary btn-rounded mb-4 fs-5" role="button" type="submit">Submit</button>
+                </div>
+                
+                
+            </form>
+
         </div>
     );
 }

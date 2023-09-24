6cc2d6817aa01c39b973e3c13915fbd851819c2c
Owen Murphree <90977322+shhhmatic@users.noreply.github.com>
2023-09-24 15:52:56 -0400

    Update AdminDashboard.jsx

--- src/AdminDashboard.jsx
+++ src/AdminDashboard.jsx
@@ -1,39 +1,66 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { getUserInfoFunction, setUserInfoFunction } from "./MongoDbClient";
-import {
-  MDBBtn,
-  MDBCardTitle,
-  MDBContainer,
-  MDBInput,
-} from "mdb-react-ui-kit";
+import { useNavigate } from 'react-router-dom';
+import {CreateUser,LoginFunction,DisplayUsers,getUserInfoFunction,setUserInfoFunction,} from "./MongoDbClient";
+import {MDBBtn,MDBContainer,MDBRow,MDBCol,MDBCard,MDBCardBody,MDBInput,MDBCardText,MDBCardTitle,} from 'mdb-react-ui-kit';
 
 const AdminDashboard = () => {
   const navigate = useNavigate();
-  const [username, setUsername] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [newIsAdmin, setNewIsAdmin] = useState(false);
-  const [newIsManager, setNewIsManager] = useState(false);
-  const [newIsActive, setNewIsActive] = useState(true);
-  const [newBadLogins, setNewBadLogins] = useState(0);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [newUsername, setNewUsername] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [newIsAdmin, setIsAdmin] = useState();
+  const [newIsManager, setIsManager] = useState();
+  const [newIsActive, setIsActive] = useState();
+  const [newBadLogins, setBadLogins] = useState();
 
   const [isUserInfoVisible, setUserInfoVisible] = useState(false);
+
+  async function getUserInfoFromServer(username) {
+    const response = await getUserInfoFunction(username);
+    // Handle the response here
+  }
+
+  async function setUserInfo(username, newPassword) {
+    // Handle setting user info here
+  }
+
+  {/*
+  function clearUserInput() {
+    setUsername('');
+    setPassword('');
+    setNewUsername('');
+    setNewPassword('');
+    setIsAdmin(false);
+    setIsManager(false);
+    setIsActive(true);
+    setBadLogins(0);
+  }
+*/}
+  function toggleUserInfoForm() {
+    const userInfoForm = document.getElementById('userInfoForm');
+    userInfoForm.style.display = "block";
+    setUserInfoVisible(true);
+  }
 
   async function getUserButton(e) {
     e.preventDefault();
     const response = await getUserInfoFunction(username);
 
     if (response.message === "User found!") {
+      toggleUserInfoForm();
+      setNewUsername(response._id || "");
       setNewPassword(response.password || "");
-      setNewIsAdmin(response.isAdmin || false);
-      setNewIsManager(response.isManager || false);
-      setNewIsActive(response.isActive || true);
-      setNewBadLogins(response.badLogins || 0);
-      setUserInfoVisible(true);
-    } else {
-      window.alert(response.message);
+      setIsAdmin(response.isAdmin);
+      setIsManager(response.isManager);
+      setIsActive(response.isActive);
+      setBadLogins(response.badLogins);
     }
+
+    window.alert(response.message);
   }
+
+
 
   async function setUserButton(e) {
     e.preventDefault();
@@ -50,15 +77,12 @@ const AdminDashboard = () => {
 
   return (
     <div>
-      <button type="button" onClick={() => navigate("/")}>
-        Home Page
-      </button>
+      <button type="button" onClick={() => navigate("/")}>Home Page</button>
 
       <h1>Admin Dashboard</h1>
 
       <p>
-        This is the admin dashboard page. System admins can complete
-        admin-specific tasks and abilities from here.
+        This is the admin dashboard page. System admins can complete admin-specific tasks and abilities from here.
       </p>
 
       <p>Insert username below to make changes.</p>
@@ -70,83 +94,30 @@ const AdminDashboard = () => {
             className="mb-2"
             type="text"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={e => setUsername(e.target.value)}
             required
           />
           <MDBCardTitle>
-            <MDBBtn
-              outline
-              color="info"
-              className="mx-3 mb-2"
-              type="submit"
-            >
+            <MDBBtn outline color="info" className="mx-3 mb-2" type="submit">
               Edit User Info
             </MDBBtn>
           </MDBCardTitle>
         </div>
       </form>
 
-      <form
-        id="userInfoForm"
-        onSubmit={setUserButton}
-        style={{ display: isUserInfoVisible ? "block" : "none" }}
-      >
-        <MDBInput
-          label="Password"
-          className="mb-2"
-          group
-          type="text"
-          validate
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-          required
-        />
-        <div className="mb-2">
-          <label>
-            Admin Privilege
-            <input
-              type="checkbox"
-              checked={newIsAdmin}
-              onChange={(e) => setNewIsAdmin(e.target.checked)}
-            />
-          </label>
-        </div>
-        <div className="mb-2">
-          <label>
-            Manager Privilege
-            <input
-              type="checkbox"
-              checked={newIsManager}
-              onChange={(e) => setNewIsManager(e.target.checked)}
-            />
-          </label>
-        </div>
-        <div className="mb-3">
-          <label>
-            Account Activated
-            <input
-              type="checkbox"
-              checked={newIsActive}
-              onChange={(e) => setNewIsActive(e.target.checked)}
-            />
-          </label>
-        </div>
-        <MDBInput
-          label="Incorrect Logins"
-          className="mb-4"
-          group
-          type="number"
-          validate
-          value={newBadLogins}
-          onChange={(e) => setNewBadLogins(parseInt(e.target.value))}
-          required
-        />
+      <form id="userInfoForm" onSubmit={setUserButton} style={{ display: isUserInfoVisible ? "block" : "none" }}>
+        <MDBInput label="Username" className="mb-2" group type="text" validate error="wrong" success="right" value={newUsername} onChange={e => setNewUsername(e.target.value)} required />
+        <MDBInput label="Password" className="mb-2" group type="text" validate value={newPassword} onChange={e => setNewPassword(e.target.value)} required />
+        <MDBInput label="Admin Privilege" className="mb-2" group type="text" validate value={newIsAdmin} onChange={e => setIsAdmin(e.target.value === "true")} required/>
+        <MDBInput label="Manager Privilege" className="mb-2" group type="text" validate value={newIsManager} onChange={e => setIsManager(e.target.value === "true")} required />
+        <MDBInput label="Account Activated" className="mb-3" group type="text" validate value={newIsActive} onChange={e => setIsActive(e.target.value === "true")} required />
+        <MDBInput label="Incorrect Logins" className="mb-4" group type="number" validate value={newBadLogins} onChange={e => setBadLogins(parseInt(e.target.value))} required />
         <MDBBtn outline color="success" type="submit">
           Save Changes
         </MDBBtn>
       </form>
     </div>
   );
-};
+}
 
 export default AdminDashboard;

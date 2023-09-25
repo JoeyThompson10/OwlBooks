b4757aaebb1bc4d3b763b35bb12dd3818189805d
JoeyThompson10 <108598229+JoeyThompson10@users.noreply.github.com>
2023-09-25 02:17:43 -0400

    Create ResetPassword.jsx

---
+++ src/ResetPassword.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ResetPassword = () => {
+    return (
+        <div>
+            <h1>
+                Reset Password
+            </h1>
+
+            <p>
+                This is the password reset page.
+            </p>
+        </div>
+    );
+}
+
+export default ResetPassword;

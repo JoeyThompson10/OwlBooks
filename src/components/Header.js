c45763dd40141c1e79fd3ed086731f4ed70b2959
JoeyThompson10 <108598229+JoeyThompson10@users.noreply.github.com>
2023-10-15 20:07:50 -0400

    Update header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -45,46 +45,51 @@ export default function Header() {
 
   return (
     <>
-      <MDBNavbar className="mx-auto py-1 bg-dark bg-gradient">
-        <MDBNavbarNav>
-          <MDBNavbarBrand className="h-100 d-flex flex-row justify-content-between align-items-center">
+        <MDBNavbar className="mx-auto py-1 bg-dark bg-gradient">
+            <MDBNavbarNav className="w-100 d-flex flex-row align-items-center">
 
-            {/* Menu Button and Sidebar */}
-            <div>
-              <Tooltip title="Open Menu">
-                <IconButton onClick={handleDrawerOpen} style={{ color: 'white' }}>
-                  <MenuIcon />
-                </IconButton>
-              </Tooltip>
-              <Drawer variant="persistent" anchor="left" open={open}>
-                <Sidebar open={open} handleDrawerClose={handleDrawerClose} isAdmin={isAdmin} />
-              </Drawer>
-            </div>
+                {/* Left Section - Menu Button, Calendar */}
+                <div className="d-flex align-items-center">
+                    <Tooltip title="Open Menu">
+                        <IconButton onClick={handleDrawerOpen} style={{ color: 'white', marginRight: '15px' }}>
+                            <MenuIcon />
+                        </IconButton>
+                    </Tooltip>
 
-            {/* Calendar */}
-            <CalendarPopover />
+                    <div>
+                        <CalendarPopover />
+                    </div>
+                </div>
 
-            {/* Logo */}
-            <Tooltip title="Go to Home">
-              <img
-                src="/assets/img/AppLogo.png"
-                alt="Logo"
-                height="65"
-                onClick={() => navigate("/")}
-              />
-            </Tooltip>
+                {/* Center Section - Logo */}
+                <div className="flex-grow-1 d-flex justify-content-center">
+                    <MDBNavbarBrand className="m-0 p-0">
+                        <Tooltip title="Go to Home">
+                            <img
+                                src="/assets/img/AppLogo.png"
+                                alt="Logo"
+                                height="65"
+                                onClick={() => navigate("/")}
+                            />
+                        </Tooltip>
+                    </MDBNavbarBrand>
+                </div>
 
-            {/* User Greeting and Options */}
-            <div className="d-flex align-items-center">
-              <Tooltip title="Your Username">
-                <p className="m-0 mr-2" style={{ color: 'white' }}>Hi, {username}</p>
-              </Tooltip>
-              <UserOptions logOut={logOut} navigateToDashboard={navigateToDashboard} />
-            </div>
+                {/* Right Section - User Greeting and Options */}
+                <div className="d-flex align-items-center">
+                    <Tooltip title="Your Username">
+                        <p className="m-0 mr-2" style={{ color: 'white' }}>Hi, {username}</p>
+                    </Tooltip>
+                    
+                    <UserOptions logOut={logOut} navigateToDashboard={navigateToDashboard} />
+                </div>
 
-          </MDBNavbarBrand>
-        </MDBNavbarNav>
-      </MDBNavbar>
+            </MDBNavbarNav>
+        </MDBNavbar>
+        <Drawer variant="persistent" anchor="left" open={open}>
+            <Sidebar open={open} handleDrawerClose={handleDrawerClose} isAdmin={isAdmin} />
+        </Drawer>
     </>
-  );
+);
+
 }

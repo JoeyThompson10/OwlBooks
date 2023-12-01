e0cc673d18e146eaef3e575f4b965b4e5173cbf8
JoeyThompson10 <108598229+JoeyThompson10@users.noreply.github.com>
2023-11-30 23:44:22 -0500

    Ads trial balance to dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -50,6 +50,7 @@ const Dashboard = () => {
         { text: 'All Accounts', icon: <AccountBalanceIcon />, path: '/allaccounts' },
         { text: 'Account Event Log', icon: <ReceiptLongIcon />, path: '/accounteventlog' },
         { text: 'Journal', icon: <AccountBalanceIcon />, path: "/journal" },
+        { text: 'Trial Balance', icon: <AccountBalanceIcon />, path: "/trialbalance"}
     ];
 
     return (

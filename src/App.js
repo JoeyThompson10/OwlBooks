5837343b92c150c23715a93ee3b09928d9d25478
Joey Thompson <JoeyThompson10@gmail.com>
2023-12-05 15:11:40 -0500

    Ads comments to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+/**
+ * Project Title: OwlBooks
+ * Team Members: Joey Thompson, Denice Jaquez, Owen Murphree, Moreland Walthour
+ * Instructor: David Burns
+ * Course: SWE 4713 SWE Application Domain
+ * 
+ * This file is part of the OwlBooks project, a web application for managing book-related activities.
+ * It sets up the main React component, App, which includes routing configuration for the application.
+ */
+
+// Importing React library, router components, and custom hooks from 'react-router-dom'.
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+
+// Importing custom header, footer, and page components for the application.
 import Header from './components/Header';
 import IntroHeader from './components/IntroHeader';
 import Footer from './components/Footer';
@@ -8,8 +21,11 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import ResetPassword from './pages/ResetPassword';
 import Profile from './pages/Profile';
+
+// Importing global styling to ensure a consistent look and feel across the application.
 import './css/global.css';
-//Routes header
+
+// Admin Dashboard related page imports.
 import EditUser from './pages/AdminDashboard/EditUser';
 import CreateUserPage from './pages/AdminDashboard/CreateUserPage';
 import AllUsers from './pages/AdminDashboard/AllUsers';
@@ -22,21 +38,25 @@ import LedgerPage from './pages/AdminDashboard/LedgerPage';
 import Journal from './pages/Journal';
 import TrialBalance from './pages/TrialBalance';
 
+// The Content component orchestrates the layout and routing of the application.
 function Content() {
-  const location = useLocation(); // Get the current location
+  // Using the useLocation hook to access the current URL path.
+  const location = useLocation();
   const currentPath = location.pathname.slice(1);
 
+  // Function to determine if the standard or introductory header should be displayed.
   const shouldDisplayHeader = () => {
     const noHeaderPaths = ["/", "/login", "/App", "/resetpassword"];
     return !noHeaderPaths.includes(location.pathname);
   };
 
-
+  // Rendering layout with conditional headers, main application routes, and footer.
   return (
     <div>
       {shouldDisplayHeader() && <Header />}
       {!shouldDisplayHeader() && <IntroHeader />}
       <Routes>
+        {/* Route configuration for each page in the OwlBooks application. */}
         <Route path="/" element={<SplashScreen />} />
         <Route path="/login" element={<Login />} />
         <Route path="/resetpassword" element={<ResetPassword />} />
@@ -53,12 +73,13 @@ function Content() {
         <Route path="/ledger/:accountId" element={<LedgerPage />} />
         <Route path="/journal" element={<Journal />} />
         <Route path="/trialbalance" element={<TrialBalance />} />
-
       </Routes>
       <Footer currentPath={currentPath} />
     </div>
   );
 }
+
+// The App component is the root component that uses Router to enable navigation.
 function App() {
   return (
     <Router>
@@ -67,4 +88,5 @@ function App() {
   );
 }
 
+// Exporting the App component for integration into the application.
 export default App;
